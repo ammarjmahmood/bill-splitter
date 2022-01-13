09f845c695b3689b4881f222cc1661403bc630dc
Ammar Jaleel Mahmood <85653702+ammarjmahmood@users.noreply.github.com>
2022-01-12 23:17:39 -0500

    python bill splitter final

---
+++ bill-splitter.py
@@ -0,0 +1,6 @@
+bill_total = int(input("Bill total: "))
+num_people = int(input("Number of people: "))
+
+print("The bill has been split", num_people, "ways")
+print("Each person will have to pay", bill_total/num_people, "$")
+print("is git working")
